b70fa676d968474d395128b82d082ed1e76dd523
James Duecaster <jamesduecaster@gmail.com>
2016-09-16 13:53:18 -0400

    update

--- taxguides/digital/cdn/dtg-csom.js
+++ taxguides/digital/cdn/dtg-csom.js
@@ -1,7 +1,7 @@
 
 /**
  * EY Digital Tax Guide - 2016 edition JavaScript
- * last update: 1 Sep 2016 2:06 PM - JD
+ * last update: 16 Sep 2016 1:51 PM - JD
  */
 
-$.getScript('https://dl.dropboxusercontent.com/u/767429/ey/taxguides/digital/js/dtg.js');
+$.getScript('https://jamesduecaster.github.io/taxguides/digital/js/dtg-csom.js');
