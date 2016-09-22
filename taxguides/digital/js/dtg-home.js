5fbcf3ed60f773a51d8d05cda661bdf681951c10
James Duecaster <jamesduecaster@gmail.com>
2016-09-22 12:02:05 -0400

    update

--- taxguides/digital/js/dtg-home.js
+++ taxguides/digital/js/dtg-home.js
@@ -1,6 +1,6 @@
 /**
  * EY Digital Tax Guide - 2016 edition JavaScript
- * last update: 22 Sep 2016 11:57 AM - JD
+ * last update: 22 Sep 2016 12:01 PM - JD
  */
 
 $(document).ready(function(){
@@ -8,8 +8,11 @@ $(document).ready(function(){
   $('[data-iso]').on('click', function(){
 
       var CCTaxGuideCountryISO = $(this).attr('data-iso');
+      var CCTaxGuideCountryName = $(this).html();
 
-      var baseHREF = 'gl/en/services/tax/ey-digital-tax-guide---cloud-service-operating-model';
+      $('.country-name').html(CCTaxGuideCountryName);
+
+      var baseHREF = '/gl/en/services/tax/ey-digital-tax-guide---cloud-service-operating-model';
 
       $('.commissioned-agent-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissioned%20agent');
       $('.commissionaire-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissionaire');
