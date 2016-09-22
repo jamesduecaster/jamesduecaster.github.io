aa15f274f7e53869f45011924ab30d2115baf68c
James Duecaster <jamesduecaster@gmail.com>
2016-09-22 11:58:41 -0400

    update

--- taxguides/digital/js/dtg-home.js
+++ taxguides/digital/js/dtg-home.js
@@ -1,22 +1,19 @@
 /**
  * EY Digital Tax Guide - 2016 edition JavaScript
- * last update: 22 Sep 2016 11:43 AM - JD
+ * last update: 22 Sep 2016 11:57 AM - JD
  */
-
-
-var CCTaxGuideCountryISO;
 
 $(document).ready(function(){
 
   $('[data-iso]').on('click', function(){
 
-      CCTaxGuideCountryISO = $(this).attr('data-iso');
+      var CCTaxGuideCountryISO = $(this).attr('data-iso');
 
       var baseHREF = 'gl/en/services/tax/ey-digital-tax-guide---cloud-service-operating-model';
 
-      $('.commissioned-agent-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + 'Commissioned%20agent');
-      $('.commissionaire-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + 'Commissionaire');
-      $('.buy-sell-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + 'Buy-sell');
+      $('.commissioned-agent-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissioned%20agent');
+      $('.commissionaire-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissionaire');
+      $('.buy-sell-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Buy-sell');
 
   });
 
