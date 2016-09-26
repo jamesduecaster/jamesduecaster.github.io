307e264e9222321959b58277591de9c9dfc41aed
James Duecaster <jamesduecaster@gmail.com>
2016-09-26 12:27:40 -0400

    update

---
+++ taxguides/digital/js/dtg-control.js
@@ -0,0 +1,81 @@
+/**
+ * EY Digital Tax Guide - Control file - 2016 edition JavaScript
+ * last update: 26 Sep 2016 12:23 PM - JD
+ */
+
+dtg.control = {
+    "techscenarios": [{
+        "scenario1": {
+            "title": "Digital advertising",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-advertising"
+        },
+        "scenario2": {
+            "title": "Payment processing",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---payment-processing"
+        },
+        "scenario3": {
+            "title": "Streaming media",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---streaming-media"
+        },
+        "scenario4": {
+            "title": "Ride sharing",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---ride-sharing"
+        },
+        "scenario5": {
+            "title": "Digital auto design",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-auto-design"
+        },
+        "scenario6": {
+            "title": "Remote auto maintenance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---remote-auto-maintenance"
+        },
+        "scenario7": {
+            "title": "Mobile banking",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---mobile-banking"
+        },
+        "scenario8": {
+            "title": "Crowdfunding",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---crowdfunding"
+        },
+        "scenario9": {
+            "title": "Wearable tech",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---wearable-tech"
+        },
+        "scenario10": {
+            "title": "Online retailer",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---online-retailer"
+        },
+        "scenario11": {
+            "title": "Supply chain reinvention",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---supply-chain-reinvention"
+        },
+        "scenario12": {
+            "title": "Drones in business",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---drones-in-business"
+        },
+        "scenario13": {
+            "title": "Media content",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---media-content"
+        },
+        "scenario14": {
+            "title": "Travel insurance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---travel-insurance"
+        },
+        "scenario15": {
+            "title": "Personal accident insurance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---personal-accident-insurance"
+        },
+        "scenario16": {
+            "title": "Digital value-based care initiatives",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-value-based-care-initiatives"
+        },
+        "scenario17": {
+            "title": "Health care data platform",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---health-care-data-platform"
+        },
+        "scenario18": {
+            "title": "Medical software",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---medical-software"
+        }
+    }]
+}
