abf319d9c93b372828d52d603a40922d2b7f9d3b
James Duecaster <jamesduecaster@gmail.com>
2016-09-27 11:31:06 -0400

    update

--- taxguides/digital/js/dtg-ts.js
+++ taxguides/digital/js/dtg-ts.js
@@ -1,6 +1,6 @@
 /**
  * EY Digital Tax Guide - Technology scenario - 2016 edition JavaScript
- * last update: 27 Sep 2016 11:28 AM - JD
+ * last update: 27 Sep 2016 11:30 AM - JD
  */
 
 var isLocal = location.href.indexOf("localhost") >= 0 || location.href.indexOf("C:/") >= 0;
@@ -211,6 +211,83 @@ dtg.onScrollInit = function(items, trigger) {
     });
 }
 
+dtg.control = {
+    "scenarios": [{
+        "scenario1": {
+            "title": "Digital advertising",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-advertising"
+        },
+        "scenario2": {
+            "title": "Payment processing",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---payment-processing"
+        },
+        "scenario3": {
+            "title": "Streaming media",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---streaming-media"
+        },
+        "scenario4": {
+            "title": "Ridesharing",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---ridesharing"
+        },
+        "scenario5": {
+            "title": "Digital auto design",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-auto-design"
+        },
+        "scenario6": {
+            "title": "Remote auto maintenance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---remote-auto-maintenance"
+        },
+        "scenario7": {
+            "title": "Mobile banking",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---mobile-banking"
+        },
+        "scenario8": {
+            "title": "Crowdfunding",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---crowdfunding"
+        },
+        "scenario9": {
+            "title": "Wearable tech",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---wearable-tech"
+        },
+        "scenario10": {
+            "title": "Online retailer",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---online-retailer"
+        },
+        "scenario11": {
+            "title": "Supply chain reinvention",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---supply-chain-reinvention"
+        },
+        "scenario12": {
+            "title": "Drones in business",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---drones-in-business"
+        },
+        "scenario13": {
+            "title": "Media content",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---media-content"
+        },
+        "scenario14": {
+            "title": "Travel insurance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---travel-insurance"
+        },
+        "scenario15": {
+            "title": "Personal accident insurance",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---personal-accident-insurance"
+        },
+        "scenario16": {
+            "title": "Digital value-based care initiatives",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---digital-value-based-care-initiatives"
+        },
+        "scenario17": {
+            "title": "Health care data platform",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---health-care-data-platform"
+        },
+        "scenario18": {
+            "title": "Medical software",
+            "link": "/gl/en/services/tax/ey-digital-tax-guide---technology-scenario---medical-software"
+        }
+    }]
+}
+
 $(document).ready(function() {
 
     $('.eyhero').addClass('reduce-height');
@@ -315,5 +392,3 @@ $(document).ready(function() {
     $(window).hashchange();
 
 });
-
-$.getScript('https://jamesduecaster.github.io/taxguides/digital/js/dtg-control.js');
