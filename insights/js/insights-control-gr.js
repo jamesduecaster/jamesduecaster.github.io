0eabceca3ca48ca2ddf65e497e053540d14bc9cf
James Duecaster <jamesduecaster@gmail.com>
2016-09-30 18:46:23 -0400

    update

---
+++ insights/js/insights-control-gr.js
@@ -0,0 +1,246 @@
+eyInsights.control = {
+        "generic": {
+            "hero-image": "/Media/vwLUExtFile/Insights-assets/$FILE/hero3.jpg",
+            "hero-image-copy": "Bringing crystal-clear insights to your business issues",
+            "intro-copy": "<p class=\"intro\">View EY insights from the listing below.</p>"
+        },
+        "navigation": [{
+            "title": "Recent insights",
+            "link": "/GR/en/Issues/Insights---DEV#recent-insights",
+            "id": "insights-recent-insights",
+            "intro-copy": ""
+        }, {
+            "title": "Browse by role",
+            "link": "/GR/en/Issues/Insights---DEV#browse-by-role",
+            "id": "insights-browse-by-role",
+            "intro-copy": "<h1>By role</h1>",
+            "subnav": [{
+                "title": "Audit committee member",
+                "link": "http://www.ey.com/GL/en/Issues/Governance-and-reporting/EY-center-for-board-matters#audit-committee",
+                "id": "insights-audit-committee-member",
+                "intro-copy": "<h1>Audit committee member</h1>"
+            }, {
+                "title": "Board member",
+                "link": "http://www.ey.com/GL/en/Issues/Governance-and-reporting/EY-center-for-board-matters",
+                "id": "insights-board-member",
+                "intro-copy": "<h1>Board member</h1>"
+            }, {
+                "title": "CDO",
+                "link": "/GL/en/Services/Transactions/Corporate-Development",
+                "id": "insights-cdo",
+                "intro-copy": "<h1>CDO</h1>"
+            }, {
+                "title": "CEO",
+                "link": "/GR/en/Issues/Insights---DEV#ceo",
+                "id": "insights-ceo",
+                "intro-copy": "<h1>CEO</h1>"
+            }, {
+                "title": "CFO",
+                "link": "/GL/en/Issues/Managing-finance/CFO_overview?utm_source=ey.com%20Managing%20finance&utm_medium=Web%20promo&utm_campaign=General",
+                "id": "insights-cfo",
+                "intro-copy": "<h1>CFO</h1>"
+            }, {
+                "title": "CIO",
+                "link": "/GL/en/Services/Advisory/EY-CIO-program",
+                "id": "insights-cio",
+                "intro-copy": "<h1>CIO</h1>"
+            }, {
+                "title": "COO",
+                "link": "/GL/en/Services/Advisory/COO-program",
+                "id": "insights-coo",
+                "intro-copy": "<h1>COO</h1>"
+            }, {
+                "title": "CRO",
+                "link": "/GR/en/Issues/Insights---DEV#cro",
+                "id": "insights-cro",
+                "intro-copy": "<h1>CRO</h1>"
+            }, {
+                "title": "CSMO",
+                "link": "/GL/en/Services/Advisory/EY-csmo-program",
+                "id": "insights-csmo",
+                "intro-copy": "<h1>CSMO</h1>"
+            }, {
+                "title": "HR director",
+                "link": "/GR/en/Issues/Insights---DEV#hr-director",
+                "id": "insights-hr-director",
+                "intro-copy": "<h1>HR director</h1>"
+            }, {
+                "title": "Tax director",
+                "link": "/GL/en/Services/Tax",
+                "id": "insights-tax-director",
+                "intro-copy": "<h1>Tax director</h1>"
+            }]
+        }, {
+            "title": "Browse by topic",
+            "link": "/GR/en/Issues/Insights---DEV#browse-by-topic",
+            "id": "insights-browse-by-topic",
+            "intro-copy": "<h1>By topic</h1>",
+            "subnav": [{
+                "title": "Accelerating growth",
+                "link": "/GL/en/Issues/Driving-growth/EY-mm-accelerating-growth",
+                "id": "insights-accelerating-growth",
+            "intro-copy": "<h1>Accelerating growth</h1>"
+            }, {
+                "title": "Analytics and big data",
+                "link": "/UK/en/Services/Specialty-Services/Big-Data",
+                "id": "insights-analytics-and-big-data",
+                "intro-copy": "<h1>Analytics and big data</h1>"
+            }, {
+                "title": "Capital allocation",
+                "link": "/UK/en/Services/Transactions",
+                "id": "insights-capital-allocation",
+                "intro-copy": "<h1>Capital allocation</h1>"
+            }, {
+                "title": "Cybersecurity",
+                "link": "/GR/en/Issues/Insights---DEV#cybersecurity",
+                "id": "insights-cybersecurity",
+                "intro-copy": "<h1>Cybersecurity</h1>"
+            }, {
+                "title": "Digital",
+                "link": "/GR/en/Issues/Insights---DEV#digital",
+                "id": "insights-digital",
+                "intro-copy": "<h1>Digital</h1>"
+            }, {
+                "title": "Emerging markets",
+                "link": "http://emergingmarkets.ey.com",
+                "id": "insights-emerging-markets",
+                "intro-copy": "<h1>Emerging markets</h1>"
+            }, {
+                "title": "EU legislation",
+                "link": "/UK/en/Services/Assurance/EY-european-union-audit-reform",
+                "id": "insights-eu-legislation",
+                "intro-copy": "<h1>EU legislation</h1>"
+            }, {
+                "title": "EY ITEM club",
+                "link": "/GR/en/Issues/Business-environment/Financial-markets-and-economy",
+                "id": "insights-ey-item-club",
+                "intro-copy": "<h1>EY ITEM club</h1>"
+            }, {
+                "title": "Eurozone Forecast",
+                "link": "/GL/en/Issues/Business-environment/EUROZONE",
+                "id": "insights-eurozone-forecast",
+                "intro-copy": "<h1>Eurozone Forecast</h1>"
+            }, {
+                "title": "EY Entrepreneur Of The Year<sup>®</sup>",
+                "link": "/UK/en/About-us/Entrepreneurship/Entrepreneur-Of-The-Year",
+                "id": "insights-ey-entrepreneur-of-the-year",
+                "intro-copy": "<h1>EY Entrepreneur Of The Year<sup>®</sup></h1>"
+            }, {
+                "title": "EY Strategic Growth Forum<sup>®</sup>",
+                "link": "/GL/en/Services/Strategic-Growth-Markets/Strategic-Growth-Forum-events",
+                "id": "insights-ey-strategic-growth-forum",
+                "intro-copy": "<h1>EY Strategic Growth Forum<sup>®</sup></h1>"
+            }, {
+                "title": "Foreign direct investment",
+                "link": "/GL/en/Issues/Business-environment/Ernst---Young-attractiveness-surveys",
+                "id": "insights-foreign-direct-investment",
+                "intro-copy": "<h1>Foreign direct investment</h1>"
+            }, {
+                "title": "Funding and finance",
+                "link": "/GR/en/Issues/Managing-finance",
+                "id": "insights-funding-and-finance",
+                "intro-copy": "<h1>Funding and finance</h1>"
+            }, {
+                "title": "IFRS",
+                "link": "/GL/en/Issues/IFRS",
+                "id": "insights-ifrs",
+                "intro-copy": "<h1>IFRS</h1>"
+            }, {
+                "title": "Infrastructure",
+                "link": "/UK/en/Services/Specialty-Services/Infrastructure",
+                "id": "insights-infrastructure",
+                "intro-copy": "<h1>Infrastructure</h1>"
+            }, {
+                "title": "Innovation",
+                "link": "/GL/en/Services/Advisory/Advisory---Innovation",
+                "id": "insights-innovation",
+                "intro-copy": "<h1>Innovation</h1>"
+            }, {
+                "title": "Mergers and acquisitions",
+                "link": "/GL/en/Services/Transactions/EY-capital-confidence-barometer",
+                "id": "insights-mergers-and-acquisitions",
+                "intro-copy": "<h1>Mergers and acquistions</h1>"
+            }, {
+                "title": "Operational effectiveness",
+                "link": "/GR/en/Issues/Insights---DEV#operational-effectiveness",
+                "id": "insights-operational-effectiveness",
+                "intro-copy": "<h1>Operational effectiveness</h1>"
+            }, {
+                "title": "Public policy",
+                "link": "/GL/en/Issues/Governance-and-reporting/Public-policy",
+                "id": "insights-public-policy",
+                "intro-copy": "<h1>Public policy</h1>"
+            }, {
+                "title": "Reporting",
+                "link": "/GR/en/Issues/Insights---DEV#reporting",
+                "id": "insights-reporting",
+                "intro-copy": "<h1>Reporting</h1>"
+            }, {
+                "title": "Risk",
+                "link": "/GR/en/Issues/Insights---DEV#risk",
+                "id": "insights-risk",
+                "intro-copy": "<h1>Risk</h1>"
+            }, {
+                "title": "Transformation",
+                "link": "/GL/en/Services/Advisory/Advisory---Transformation",
+                "id": "insights-transformation",
+                "intro-copy": "<h1>Transformation</h1>"
+            }, {
+                "title": "Women in business",
+                "link": "/GR/en/Issues/Insights---DEV#women-in-business",
+                "id": "insights-women-in-business",
+                "intro-copy": "<h1>Women in business</h1>"
+            },{
+                "title": "5: insights for executives",
+                "link": "/GL/en/Services/Advisory/5-insights-for-executives-series",
+                "id": "insights-5--insights-for-executives",
+                "intro-copy": "<h1>5: insights for executives</h1>"
+            }]
+        },  {
+            "title": "Browse by megatrends",
+            "link": "/GR/en/Issues/Insights---DEV#browse-by-megatrends",
+            "id": "insights-browse-by-megatrends",
+            "intro-copy": "<h1>By megatrends</h1>",
+            "subnav": [{
+                "title": "Behavioral revolution",
+                "link": "/GR/en/Issues/Insights---DEV#behavioral-revolution",
+                "id": "insights-behavioral-revolution",
+                "intro-copy": "<h1>Behavioral revolution</h1>"
+            }, {
+                "title": "Empowered customer",
+                "link": "/GR/en/Issues/Insights---DEV#empowered-customer",
+                "id": "insights-empowered-customer",
+                "intro-copy": "<h1>Empowered customer</h1>"
+            }, {
+                "title": "Health reimagined",
+                "link": "/GR/en/Issues/Insights---DEV#health-reimagined",
+                "id": "insights-health-reimagined",
+                "intro-copy": "<h1>Health reimagined</h1>"
+            }, {
+                "title": "Industry redefined",
+                "link": "/GR/en/Issues/Insights---DEV#industry-redefined",
+                "id": "insights-industry-redefined",
+                "intro-copy": "<h1>Industry redefined</h1>"
+            }, {
+                "title": "Resourceful planet",
+                "link": "/GR/en/Issues/Insights---DEV#resourceful-planet",
+                "id": "insights-resourceful-planet",
+                "intro-copy": "<h1>Resourceful planet</h1>"
+            }, {
+                "title": "The future of smart",
+                "link": "/GR/en/Issues/Insights---DEV#the-future-of-smart",
+                "id": "insights-the-future-of-smart",
+                "intro-copy": "<h1>The future of smart</h1>"
+            }, {
+                "title": "The future of work",
+                "link": "/GR/en/Issues/Insights---DEV#the-future-of-work",
+                "id": "insights-the-future-of-work",
+                "intro-copy": "<h1>The future of work</h1>"
+            }, {
+                "title": "Urban world",
+                "link": "/GR/en/Issues/Insights---DEV#urban-world",
+                "id": "insights-urban-world",
+                "intro-copy": "<h1>Urban world</h1>"
+            }]
+        }]
+}
