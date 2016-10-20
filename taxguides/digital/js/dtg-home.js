d7fc5e6aa30575241123716e1c9c19ace58e4a24
James Duecaster <jamesduecaster@gmail.com>
2016-10-20 01:05:11 -0400

    update

--- taxguides/digital/js/dtg-home.js
+++ taxguides/digital/js/dtg-home.js
@@ -1,6 +1,6 @@
 /**
  * EY Digital Tax Guide - 2016 edition JavaScript
- * last update: 19 October 2016 11:12 PM - JD
+ * last update: 19 October 2016 1:04 AM - JD
 
 
  * Analytics 2016-08-29
@@ -73,15 +73,20 @@ $(document).ready(function() {
         var CCTaxGuideCountryISO = $(this).attr('data-iso');
         var CCTaxGuideCountryName = $(this).html();
 
+        Analytics.TrackEvent('Home - CC Tax info country selection', 'Click', CCtaxGuideCountryName);
+
         $('.country-name').html(CCTaxGuideCountryName);
 
         var baseHREF = '/gl/en/services/tax/ey-digital-tax-guide---cloud-service-operating-model';
         var baseHREFTaxBasics = '/gl/en/services/tax/ey-digital-tax-guide---tax-basics';
 
-        $('.corp-vat-basics-link').attr('href', baseHREFTaxBasics + '#' + CCTaxGuideCountryISO);
-        $('.commissioned-agent-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissioned%20agent');
-        $('.commissionaire-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Commissionaire');
-        $('.buy-sell-link').attr('href', baseHREF + '#' + CCTaxGuideCountryISO + '-Buy-sell');
+        $('.corp-vat-basics-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Tax technical guidance selection\', \'Click\', \'Corp. and VAT basics for ' + CCTaxGuideCountryName + '\');location.href=\'' + baseHREFTaxBasics + '#' + CCTaxGuideCountryISO + '\'');
+
+        $('.commissioned-agent-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Tax technical guidance selection\', \'Click\', \'Commissioned agent model for ' + CCTaxGuideCountryName + '\');location.href=\'' + baseHREF + '#' + CCTaxGuideCountryISO + '-Commissioned%20agent\'');
+
+        $('.commissionaire-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Tax technical guidance selection\', \'Click\', \'Commissionaire model for ' + CCTaxGuideCountryName + '\');location.href=\'' + baseHREF + '#' + CCTaxGuideCountryISO + '-Commissionaire\'');
+
+        $('.buy-sell-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Tax technical guidance selection\', \'Click\', \'Buy-sell model for ' + CCTaxGuideCountryName + '\');location.href=\'' + baseHREF + '#' + CCTaxGuideCountryISO + '-Buy-sell\'');
 
     });
 
@@ -90,6 +95,8 @@ $(document).ready(function() {
         var thisSector = $(this).attr('data-sector');
         var thisSectorName = $(this).find('h3').html();
         var scenarioBaseHREF = '/gl/en/services/tax/ey-digital-tax-guide---scenario---';
+
+        Analytics.TrackEvent('Home - Sector selection', 'Click', thisSectorName);
 
         $('.generic-modal-2-content .sector-name').html(thisSectorName);
 
@@ -100,102 +107,143 @@ $(document).ready(function() {
 
         switch (thisSector) {
             case 'automotive':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'digital-auto-design').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Digital auto design\');location.href=\'' + scenarioBaseHREF + 'digital-auto-design\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Digital auto design');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'ridesharing').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Ridesharing\');location.href=\'' + scenarioBaseHREF + 'ridesharing\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Ridesharing');
 
-                $('.generic-modal-2-content .scenario-3-link').attr('href', scenarioBaseHREF + 'remote-auto-maintenance').show();
+
+                $('.generic-modal-2-content .scenario-3-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Remote auto maintenance\');location.href=\'' + scenarioBaseHREF + 'remote-auto-maintenance\'').show();
+
                 $('.generic-modal-2-content .scenario-3-link h3').html('Remote auto maintenance');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'banking-and-finance':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'crowdfunding').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Crowdfunding\');location.href=\'' + scenarioBaseHREF + 'crowdfunding\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Crowdfunding');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'mobile-banking').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Mobile banking\');location.href=\'' + scenarioBaseHREF + 'mobile-banking\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Mobile banking');
+
 
                 $('.generic-modal-2-content .scenario-3-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-3-link h3').html('');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'consumer-products':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'online-retailer').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Online retailer\');location.href=\'' + scenarioBaseHREF + 'online-retailer\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Online retailer');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'supply-chain-reinvention').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Supply chain reinvention\');location.href=\'' + scenarioBaseHREF + 'supply-chain-reinvention\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Supply chain reinvention');
 
-                $('.generic-modal-2-content .scenario-3-link').attr('href', scenarioBaseHREF + 'wearable-tech').show();
+
+                $('.generic-modal-2-content .scenario-3-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Wearable tech\');location.href=\'' + scenarioBaseHREF + 'wearable-tech\'').show();
+
                 $('.generic-modal-2-content .scenario-3-link h3').html('Wearable tech');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'insurance':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'personal-accident-insurance').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Personal accident insurance\');location.href=\'' + scenarioBaseHREF + 'personal-accident-insurance\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Personal accident insurance');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'travel-insurance').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Travel insurance\');location.href=\'' + scenarioBaseHREF + 'travel-insurance\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Travel insurance');
+
 
                 $('.generic-modal-2-content .scenario-3-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-3-link h3').html('');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'life-sciences':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'digital-value-based-care-initiatives').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Digital value-based care initiatives\');location.href=\'' + scenarioBaseHREF + 'digital-value-based care-initiatives\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Digital value-based care initiatives');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'health-care-data-platform').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Health care data platform\');location.href=\'' + scenarioBaseHREF + 'health-care-data-platform\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Health care data platform');
 
-                $('.generic-modal-2-content .scenario-3-link').attr('href', scenarioBaseHREF + 'medical-software').show();
+
+                $('.generic-modal-2-content .scenario-3-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Medical software\');location.href=\'' + scenarioBaseHREF + 'medical-software\'').show();
+
                 $('.generic-modal-2-content .scenario-3-link h3').html('Medical software');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'media-and-entertainment':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'media-content').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Media content\');location.href=\'' + scenarioBaseHREF + 'media-content\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Media content');
+
 
                 $('.generic-modal-2-content .scenario-2-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-2-link h3').html('');
 
+
                 $('.generic-modal-2-content .scenario-3-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-3-link h3').html('');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
                 break;
             case 'technology':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'digital-advertising').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Digital advertising\');location.href=\'' + scenarioBaseHREF + 'digital-advertising\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Digital advertising');
 
-                $('.generic-modal-2-content .scenario-2-link').attr('href', scenarioBaseHREF + 'payment-processing').show();
+
+                $('.generic-modal-2-content .scenario-2-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Payment processing\');location.href=\'' + scenarioBaseHREF + 'payment-processing\'').show();
+
                 $('.generic-modal-2-content .scenario-2-link h3').html('Payment processing');
 
-                $('.generic-modal-2-content .scenario-3-link').attr('href', scenarioBaseHREF + 'streaming-media').show();
+
+                $('.generic-modal-2-content .scenario-3-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Streaming media\');location.href=\'' + scenarioBaseHREF + 'streaming-media\'').show();
+
                 $('.generic-modal-2-content .scenario-3-link h3').html('Streaming media');
                 break;
             case 'telecommunications':
-                $('.generic-modal-2-content .scenario-1-link').attr('href', scenarioBaseHREF + 'drones-in-business').show();
+                $('.generic-modal-2-content .scenario-1-link').attr('href', 'javascript:Analytics.TrackEvent(\'Home - Scenario selection\', \'Click\', \'Drones in business\');location.href=\'' + scenarioBaseHREF + 'drones-in-business\'').show();
+
                 $('.generic-modal-2-content .scenario-1-link h3').html('Drones in business');
+
 
                 $('.generic-modal-2-content .scenario-2-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-2-link h3').html('');
 
+
                 $('.generic-modal-2-content .scenario-3-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-3-link h3').html('');
+
 
                 $('.generic-modal-2-content .scenario-4-link').attr('href', '#');
                 $('.generic-modal-2-content .scenario-4-link h3').html('');
@@ -206,7 +254,6 @@ $(document).ready(function() {
 });
 
 $(window).load(function(){
-
 
   var waypointSection0 = new Waypoint({
       element: $('#section0'),
